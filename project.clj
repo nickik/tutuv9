edb296b51bd82daabf39d0402982b5320e5ef7f8
Niklaus Zbinden <niklaus.zbinden@gmail.com>
2018-11-28 20:59:02 +0100

    first ts

--- project.clj
+++ project.clj
@@ -10,5 +10,6 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
+  :main clojure-getting-started.web/-main
   :uberjar-name "clojure-getting-started-standalone.jar"
   :profiles {:production {:env {:production true}}})
--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,16 +4,25 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]])
+  (:import (org.joda.time DateTime)
+           (org.joda.time.format ISODateTimeFormat)))
 
-(defn splash []
+(def alarm (atom (DateTime.)))
+
+(defn read-alarm []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :body
+    (str (. (ISODateTimeFormat/dateTime) print @alarm))})
+
+(defn set-alarm []
+  (reset! alarm (DateTime.)))
 
 (defroutes app
   (GET "/" []
-       (splash))
+    (read-alarm))
+  (POST "/" [] (set-alarm) (read-alarm))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
