c9b71711b84f8a03f671ceb0ec607090f2a90ec1
Niklaus Zbinden <niklaus.zbinden@gmail.com>
2018-12-29 19:00:18 +0100

    Advanced

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
@@ -12,6 +12,6 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :main tutuv9/-main
+  :main tutuv9.web
   :uberjar-name "tutuv9-standalone.jar"
   :profiles {:production {:env {:production true}}})
--- src/tutuv9/web.clj
+++ src/tutuv9/web.clj
@@ -6,41 +6,45 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [clojure.core.async :as async :refer [thread]]
-            [clojure.tools.logging :as log])
-  (:import (org.joda.time DateTime)))
+            [clojure.tools.logging :as log]))
 
-(def alarm (atom nil))
+(def alarm (atom #{}))
 
-(defn read-alarm []
-  {:status (if (nil? @alarm) 204 200)
+(defn read-alarm [alarm-name]
+  {:status (if (some #{alarm-name} @alarm) 204 200)
    :headers {"Content-Type" "text/plain"}
-   :body (str (if (nil? @alarm) 204 200))
-    })
+   :body (str (if (some #{alarm-name} @alarm) 204 200))})
 
-(defn get-button []
+(defn get-button [name]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (str "<html><head><title>Luxeria tutu</title></head><body><form method='POST' src='/'><input type=\"submit\" value=\"RING!\" style=\"width:100%; height: 300px;background-color: #f44336;\"></form></body></html>")
-   })
+   :body (str "<html><head><title>Luxeria tutu</title></head><body><form method='POST' src='/" name "'><input type=\"submit\" value=\"RING!\" style=\"width:100%; height: 300px;background-color: #f44336;\"></form></body></html>")})
 
 (add-watch alarm :watcher
            (fn [_ _ _ new-state]
              (when new-state
                (log/info "Alarm request sent at " (str new-state)))))
 
-(defn set-alarm []
-  (let [dt (DateTime.)]
-    (reset! alarm dt)
-    (async/thread
-      (Thread/sleep 5000)
-      (compare-and-set! alarm dt nil))))
+(defn set-alarm [alarm-name]
+  (swap! alarm conj alarm-name)
+  (async/thread
+    (Thread/sleep 5000)
+    (swap! alarm disj alarm-name)))
 
 (defroutes app
   (GET "/" []
-    (get-button))
+    (get-button ""))
   (GET "/check" []
-    (read-alarm))
-  (POST "/" [] (set-alarm) (get-button))
+    (read-alarm "default"))
+  (POST "/" [] (set-alarm "default") (get-button ""))
+
+  (GET "/:name" [name]
+    (get-button name))
+  (GET "/check/:name" [name]
+    (read-alarm name))
+  (POST "/:name" [name]
+    (set-alarm name) (get-button name))
+
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
